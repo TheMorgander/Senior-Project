7ceb60de27531c5aedfe8e1c5fc750cff5f7552a
TheMorgander <themorgander@gmail.com>
2022-02-26 15:30:09 -0800

    Further development of settings app

--- Code/Taskbar Resources/Settings/history_settings.xaml.cs
+++ Code/Taskbar Resources/Settings/history_settings.xaml.cs
@@ -24,5 +24,10 @@ namespace Settings
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
